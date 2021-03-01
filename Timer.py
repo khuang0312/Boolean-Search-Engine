02e2e577225234647bfe3e7e11498f518f538a2e
Kevin Huang <huangk11@uci.edu>
2021-02-28 22:32:02 -0800

    Made Timer class dedicated to timing the queries

---
+++ Timer.py
@@ -0,0 +1,60 @@
+from time import perf_counter
+
+class Timer:
+    def __init__(self, name : str):
+        self.name = name                # describes what it is timing             
+        self.begin = perf_counter()
+        self.end = 0                    # 0 signifies if timer has stopped
+    
+    def __str__(self):
+        return "Timer {}, {} seconds elapsed".format(self.name, self.time_elapsed())
+    
+    def __eq__(self, other):
+        '''If a timer is timing the samething as another timer
+        They are considered one and the same...
+        '''
+        return self.name == other.name
+
+    def subject(self):
+        '''Returns what the timer is timing
+        '''
+        return self.name
+    
+    def begin_and_end(self):
+        '''Returns a tuple when timer started and when timer ended or current time
+        '''
+        return (self.begin, self.end if self.end != 0 else perf_counter())
+
+    
+    def has_stopped(self) -> bool:
+        '''Timer only stops if the stop method has ben called...
+        '''
+        return self.end != 0
+
+    def stop(self):
+        '''Stops timer, the end time will be permanently the time when
+            this method is called
+        '''
+        self.end = perf_counter()
+    
+    def time_elapsed(self) -> float:
+        '''Returns fractional seconds
+        '''
+        if self.end == 0:
+            return perf_counter() - self.begin
+        return self.end - self.begin
+
+def to_milliseconds(seconds):
+    return seconds * 1000
+
+        
+if __name__ == "__main__":
+    # create your timer
+    # do whatever you need to do
+    # stop timer
+    # get timer properties as needed... 
+
+    t = Timer("search1")
+    # search1 code here
+    t.stop()
+    print(t)
